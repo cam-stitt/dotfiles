41bab87cbadfbbbdc29beec8d62c370de9dc4196
Cameron Stitt <cameron@cam.st>
2016-11-22 08:03:52 +1000

    Use aurora theme.

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -1,18 +1,20 @@
-;(setq tomorrow-theme-directory (concat user-emacs-directory "Themes/GNU Emacs"))
+(setq theme-directory (concat user-emacs-directory "Themes/GNU Emacs"))
+(setq aurora-directory (concat theme-directory "/aurora-theme"))
 ;(setq base16-theme-directory (concat user-emacs-directory "Themes/GNU Emacs/base16-emacs"))
-;(add-to-list 'load-path base16-theme-directory)
+(add-to-list 'load-path aurora-directory)
+(require 'aurora-theme)
 ;;(require 'tomorrow-night-theme)
 ;(require 'base16-default-theme)
 
-(unless (package-installed-p 'ample-theme)
-  (package-install 'ample-theme))
-(load-theme 'ample-flat t t)
-(enable-theme 'ample-flat)
+;(unless (package-installed-p 'ample-theme)
+;  (package-install 'ample-theme))
+;(load-theme 'ample-flat t t)
+;(enable-theme 'ample-flat)
 
 (setq-default indent-tabs-mode nil)
 
 (add-to-list 'default-frame-alist
-	     '(font . "Ubuntu Mono-14"))
+	     '(font . "Go Mono-14"))
 
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
